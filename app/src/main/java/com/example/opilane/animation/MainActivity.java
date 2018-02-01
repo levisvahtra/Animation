3c81d8ed7431245a71a6eaabbbb983ffbdf9b50e
Opilane TTHK <opilane@tthk.ee>
2018-02-01 11:44:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/animation/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.opilane.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void zoom(View view) {
+    }
+
+    public void clock(View view) {
+    }
+
+    public void blink(View view) {
+    }
+
+    public void move(View view) {
+    }
+
+    public void slide(View view) {
+    }
+
+    public void fade(View view) {
+    }
+}
